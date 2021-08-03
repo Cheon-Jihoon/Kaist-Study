93afb8787df6ea52148f2550edafd6a0e012a5a2
c_zhoon39@naver.com <c_zhoon39@naver.com>
2021-08-03 13:31:58 +0900

    내용 지움
    짧게하기위하여

--- 0803_study.py
+++ 0803_study.py
@@ -5,17 +5,6 @@ print("첫 번째 공부")
 
 
 
-print("첫 번째 공부")print("첫 번째 공부")
-print("첫 번째 공부")print("첫 번째 공부")print("첫 번째 공부")print("첫 번째 공부")
-print("첫 번째 공부")
-
-
-
-
-print("첫 번째 공부")print("첫 번째 공부")
-print("첫 번째 공부")print("첫 번째 공부")print("첫 번째 공부")print("첫 번째 공부")
-print("첫 번째 공부")
-
 
 
 
