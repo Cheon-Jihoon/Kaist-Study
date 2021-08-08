7b70d11c73e9d7edc9bf304597190958d3c92dec
c_zhoon39@naver.com <c_zhoon39@naver.com>
2021-08-08 15:42:29 +0900

    mystudy 파일을 수정함.

--- mystudy.py
+++ mystudy.py
@@ -1,1 +1,17 @@
-dwqdqwdwqddqwdcqwkdqw
+def is_valid(driver, registered):
+    for i in range(2):
+        if driver[i] != registered[i]:
+            return False
+    return True
+
+
+def check(driver, registered_list, arrested_list, num_suspects):
+    for i in registered_list:
+        if is_valid(driver, i):
+            return False
+        else:
+            arrested_list.append(driver)
+    if len(arrested_list) == num_suspects:
+        return True
+    else:
+        return False
